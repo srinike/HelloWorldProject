b096fbaffe419b66ed063ccf3d4b16ed60943d16
srinike <esrini@gmail.com>
2014-12-29 23:37:11 -0500

    Added new files

---
+++ LocalHelloWorldRepository/HelloWorldLunaProject/src/com/hello/client/ClientSource.java
@@ -0,0 +1,13 @@
+package com.hello.client;
+
+import com.hello.server.ServerSource;
+
+public class ClientSource {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The total of the numbers is : " + ServerSource.addNumbers(4, 5));
+
+	}
+
+}
---
+++ LocalHelloWorldRepository/HelloWorldLunaProject/src/com/hello/server/ServerSource.java
@@ -0,0 +1,7 @@
+package com.hello.server;
+
+public class ServerSource {
+	public static int addNumbers(int a, int b) {
+		return (a + b);
+	}
+}
